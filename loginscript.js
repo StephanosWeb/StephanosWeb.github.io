4ef49a3c1d3b60ea999e3c12f6a1e7997134d4c2
Stephanos Nicolaou <stephanos@Stephanoss-MacBook-Pro.local>
2018-02-06 18:59:39 +0000

    Updating site

---
+++ loginscript.js
@@ -0,0 +1,6 @@
+function login() {
+  var entery = prompt('Pleae enter the password provided by your teacher:');
+  if (pass1.toLowerCase() == 'roblox123') {
+      window.location='/downloads/TycoonExercise3.rbxl'
+  }
+}
