5ca2b9775b26c73f5f0f9704bd85453c798be26e
Stephanos Nicolaou <stephanos@Stephanoss-MacBook-Pro.local>
2018-02-07 01:19:13 +0000

    Updating site

--- loginscript.js
+++ loginscript.js
@@ -1,6 +1,7 @@
 function login() {
-  var entery = prompt('Pleae enter the password provided by your teacher:');
-  if (pass1.toLowerCase() == 'roblox123') {
-      window.location='/downloads/TycoonExercise3.rbxl'
+  var entry = prompt('Pleae enter the password provided by your teacher:');
+  if (entry.toLowerCase() == '12') {
+      console.log("inside");
+      window.location='/downloads/TycoonExercise3.rbxl';
   }
 }
