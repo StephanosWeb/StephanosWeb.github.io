5daa0ab4acf7223d47e98b23c785acfa6ed38fbd
Stephanos Nicolaou <stephanos@Stephanoss-MacBook-Pro.local>
2018-02-20 16:13:20 +0000

    Updating site

--- loginscript.js
+++ loginscript.js
@@ -1,5 +1,5 @@
 function encryption() {
-  var encryption_key = "313233726f626c6f78313233"
+  var encryption_key = "6170706C65333231"
   var passkey = ""
   for(var i=0;i<encryption_key.length;i=i+2){passkey=passkey+'%'+encryption_key.substr(i,2);}
   return unescape(passkey);
