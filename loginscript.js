1e43ed8e6c3f7d010b340fcf4086385355b9fdc0
Stephanos Nicolaou <stephanos@Stephanoss-MacBook-Pro.local>
2018-02-20 15:11:11 +0000

    Updating site

--- loginscript.js
+++ loginscript.js
@@ -1,5 +1,5 @@
 function encryption() {
-  var encryption_key = "726f626c6f78313233"
+  var encryption_key = "313233726f626c6f78313233"
   var passkey = ""
   for(var i=0;i<encryption_key.length;i=i+2){passkey=passkey+'%'+encryption_key.substr(i,2);}
   return unescape(passkey);
@@ -9,6 +9,6 @@ function login() {
   var entry = prompt('Please enter the password provided by your teacher:');
   var key = encryption();
   if (entry.toLowerCase() == key) {
-      window.location='/downloads/TycoonExercise3-Finished.rbxl';
+      window.location='/downloads/TycoonExercise4-Finished.rbxl';
   }
 }
