e30650de1d31db6a8806e2027922b7d8da6fb543
stephanos <stephanosnucas@yahoo.co.uk>
2018-06-25 11:48:52 +0100

    Updating site

--- fantasia/custom.js
+++ fantasia/custom.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-  // Initialize Tooltip
-  $('[data-toggle="tooltip"]').tooltip();
-
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+  // Add smooth scrolling to all links in navbar
+  $(".navbar a").on('click', function(event) {
 
     // Prevent default anchor click behavior
     event.preventDefault();
@@ -18,7 +15,7 @@ $(document).ready(function(){
     }, 900, function(){
 
       // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
+      // window.location.hash = hash;
     });
   });
 })
